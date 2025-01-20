f8a796cd39c1cfc6ca063da49d8edef365d50f58
Forward <Julia.Forward@k202domain.com>
2025-01-20 14:07:43 -0330

    The Josh Taylor Fumble Hour

---
+++ src/main/java/cna/nl/juliaforward/java3/JDBC/DBProperties.java
@@ -0,0 +1,11 @@
+package cna.nl.juliaforward.java3.JDBC;
+
+public class DBProperties {
+
+    public static final String DATABASE_URL = "jdbc:mysql://localhost:3306/";
+
+    public static final String DATABASE_USER = "root";
+
+    public static final String DATABASE_PASSWORD = "password";
+
+}
