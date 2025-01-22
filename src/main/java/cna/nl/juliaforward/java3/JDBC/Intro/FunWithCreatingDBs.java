1e6e9999d277f3a1123d4ebd303249446e2b5314
Forward <Julia.Forward@k202domain.com>
2025-01-22 10:11:26 -0330

    Maria DB interactions

--- src/main/java/cna/nl/juliaforward/java3/JDBC/Intro/FunWithCreatingDBs.java
+++ src/main/java/cna/nl/juliaforward/java3/JDBC/Intro/FunWithCreatingDBs.java
@@ -1,6 +1,6 @@
 package cna.nl.juliaforward.java3.JDBC.Intro;
 
-import cna.nl.juliaforward.java3.JDBC.DBProperties;
+import cna.nl.juliaforward.java3.JDBC.MariaDBProperties;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -15,10 +15,10 @@ public class FunWithCreatingDBs {
     public static void main(String[] args) {
         // Open a connection
         try(Connection conn = DriverManager.getConnection(
-                DBProperties.DATABASE_URL, DBProperties.DATABASE_USER, DBProperties.DATABASE_PASSWORD);
+                MariaDBProperties.DATABASE_URL, MariaDBProperties.DATABASE_USER, MariaDBProperties.DATABASE_PASSWORD);
             Statement stmt = conn.createStatement();
         ) {
-            String sql = "CREATE DATABASE STUDENTS";
+            String sql = "CREATE DATABASE Test";
             stmt.executeUpdate(sql);
             System.out.println("Database created successfully...");
         } catch (SQLException e) {
--- src/main/java/cna/nl/juliaforward/java3/JDBC/Intro/FunWithDrivers.java
+++ src/main/java/cna/nl/juliaforward/java3/JDBC/Intro/FunWithDrivers.java
@@ -1,5 +1,7 @@
 package cna.nl.juliaforward.java3.JDBC.Intro;
 
+import cna.nl.juliaforward.java3.JDBC.MariaDBProperties;
+
 import java.sql.*;
 
 /**
@@ -14,25 +16,10 @@ public class FunWithDrivers {
     public static void main(String[] args) {
         System.out.println("Fun trying to registers a driver");
 
-        //Option 1: Find the class
-        try {
-            Class.forName("org.mariadb.jdbc.Driver").newInstance();
-            System.out.println("Option 1: Find the class worked!");
-        } catch (ClassNotFoundException ex) {
-            System.out.println("Error: unable to load driver class!");
-        } catch (IllegalAccessException ex) {
-            System.out.println("Error: access problem while loading!");
-        } catch (InstantiationException ex) {
-            System.out.println("Error: unable to instantiate driver!");
-        }
-
-        //Option 2: Register the Driver
-        try {
-            Driver myDriver = new org.mariadb.jdbc.Driver();
-            DriverManager.registerDriver(myDriver);
-            System.out.println("Option 2: Register the Driver worked!");
-        } catch (SQLException ex) {
-            System.out.println("Error: unable to load driver class!");
+        if(MariaDBProperties.isDriverRegistered(System.err)) {
+            System.out.println("Success!");
+        } else {
+            System.out.println("Fail!");
         }
 
     }
