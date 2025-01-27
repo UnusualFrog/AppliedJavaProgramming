e479d3ae316fd5bfa232a6fcf7de89588cd3417f
Forward <Julia.Forward@k202domain.com>
2025-01-27 16:27:57 -0330

    Added CRUD methods to a01

--- src/main/java/cna/nl/juliaforward/java3/JDBC/Assignment/A01/BookApplication.java
+++ src/main/java/cna/nl/juliaforward/java3/JDBC/Assignment/A01/BookApplication.java
@@ -1,5 +1,7 @@
 package cna.nl.juliaforward.java3.JDBC.Assignment.A01;
 
+import org.checkerframework.checker.units.qual.A;
+
 public class BookApplication {
     public static void main(String[] args) {
         //BookDatabaseManager dbm = new BookDatabaseManager();
@@ -10,11 +12,19 @@ public class BookApplication {
 //        System.out.println(lib.getAuthorList());
 //        System.out.println(lib.getBookList());
 
-//        Book book = new Book("ZZZZ", "ZZZZZZZ", 99, "20XX");
+//        Book book = new Book("JJJJ", "JJJJJJJJJ", 89, "31XX");
 //        BookDatabaseManager.createBook(book);
 
-        Author author = new Author(123, "Eric", "Stock");
-        BookDatabaseManager.createAuthor(author);
+//        Author author = new Author(133, "John", "Smith");
+//        BookDatabaseManager.createAuthor(author);
 
+//        System.out.println(BookDatabaseManager.getBook("ZZZZ").getTitle());
+//        System.out.println(BookDatabaseManager.getAllBooks());
+
+//        System.out.println(BookDatabaseManager.getAuthor("5").getFirstName() +" " + BookDatabaseManager.getAuthor("5").getLastName());
+//        System.out.println(BookDatabaseManager.getAllAuthors());
+
+//        BookDatabaseManager.updateBook("GGGG", new Book("GGGG", "This is an update", 20, "XXXX"));
+        BookDatabaseManager.updateAuthor("133", new Author(133, "Mark", "Danielewski"));
     }
 }
--- src/main/java/cna/nl/juliaforward/java3/JDBC/Assignment/A01/BookDatabaseManager.java
+++ src/main/java/cna/nl/juliaforward/java3/JDBC/Assignment/A01/BookDatabaseManager.java
@@ -4,6 +4,8 @@ import cna.nl.juliaforward.java3.JDBC.MariaDBProperties;
 import org.checkerframework.checker.units.qual.A;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookDatabaseManager {
 
@@ -61,15 +63,19 @@ public class BookDatabaseManager {
         }
     }
 
+    // CREATE methods
     public static void createBook(Book book) {
-        String CREATE_BOOK_QUERY = "INSERT INTO titles VALUES (\"%s\", \"%s\", %d, \"%s\")";
-        CREATE_BOOK_QUERY = String.format(CREATE_BOOK_QUERY, book.getIsbn(),book.getTitle(), book.getEditionNumber(), book.getCopyright());
+        String CREATE_BOOK_QUERY = "INSERT INTO titles VALUES (?, ?, ?, ?)";
         System.out.println(CREATE_BOOK_QUERY);
         try {
             Connection conn = DriverManager.getConnection(
                     MariaDBProperties.DATABASE_URL + DB_NAME, MariaDBProperties.DATABASE_USER, MariaDBProperties.DATABASE_PASSWORD);
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(CREATE_BOOK_QUERY);
+            PreparedStatement pstmt = conn.prepareStatement(CREATE_BOOK_QUERY);
+            pstmt.setString(1, book.getIsbn());
+            pstmt.setString(2, book.getTitle());
+            pstmt.setInt(3, book.getEditionNumber());
+            pstmt.setString(4, book.getCopyright());
+            ResultSet rs = pstmt.executeQuery();
 
             System.out.println("Created book successfully!");
             conn.close();
@@ -80,14 +86,16 @@ public class BookDatabaseManager {
     }
 
     public static void createAuthor(Author author) {
-        String CREATE_AUTHOR_QUERY = "INSERT INTO authors VALUES (%d, \"%s\", \"%s\")";
-        CREATE_AUTHOR_QUERY = String.format(CREATE_AUTHOR_QUERY, author.getAuthorID(), author.getFirstName(), author.getLastName());
+        String CREATE_AUTHOR_QUERY = "INSERT INTO authors VALUES (?, ?, ?)";
         System.out.println(CREATE_AUTHOR_QUERY);
         try {
             Connection conn = DriverManager.getConnection(
                     MariaDBProperties.DATABASE_URL + DB_NAME, MariaDBProperties.DATABASE_USER, MariaDBProperties.DATABASE_PASSWORD);
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(CREATE_AUTHOR_QUERY);
+            PreparedStatement pstmt = conn.prepareStatement(CREATE_AUTHOR_QUERY);
+            pstmt.setInt(1, author.getAuthorID());
+            pstmt.setString(2, author.getFirstName());
+            pstmt.setString(3, author.getLastName());
+            ResultSet rs = pstmt.executeQuery();
 
             System.out.println("Created author successfully!");
             conn.close();
@@ -96,4 +104,165 @@ public class BookDatabaseManager {
             System.out.println("Error creating author");
         }
     }
+
+    // GET Methods
+    public static Book getBook(String isbn) {
+        String GET_BOOK_QUERY = "SELECT * FROM titles WHERE isbn = ?";
+        System.out.println(GET_BOOK_QUERY);
+
+        Book book = null;
+        try {
+            Connection conn = DriverManager.getConnection(
+                    MariaDBProperties.DATABASE_URL + DB_NAME, MariaDBProperties.DATABASE_USER, MariaDBProperties.DATABASE_PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement(GET_BOOK_QUERY);
+            pstmt.setString(1, isbn);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                book = new Book(rs.getString("isbn"), rs.getString("title"), rs.getInt("editionNumber"), rs.getString("copyright"));
+
+                System.out.println("Successfully retrieved book!");
+            }
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error getting book");
+        }
+
+        return book;
+    }
+
+    public static List<Book> getAllBooks() {
+        String GET_BOOKS_QUERY = "SELECT * FROM titles";
+        System.out.println(GET_BOOKS_QUERY);
+
+        List<Book> books = new ArrayList<Book>();
+        try {
+            Connection conn = DriverManager.getConnection(
+                    MariaDBProperties.DATABASE_URL + DB_NAME, MariaDBProperties.DATABASE_USER, MariaDBProperties.DATABASE_PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement(GET_BOOKS_QUERY);
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                Book book = null;
+                book = new Book(rs.getString("isbn"), rs.getString("title"), rs.getInt("editionNumber"), rs.getString("copyright"));
+                books.add(book);
+
+                System.out.println("Successfully retrieved: " + rs.getString("title"));
+            }
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error getting books");
+        }
+
+        return books;
+    }
+
+    public static Author getAuthor(String authorID) {
+        String GET_AUTHOR_QUERY = "SELECT * FROM authors WHERE authorID = ?";
+        System.out.println(GET_AUTHOR_QUERY);
+
+        Author author = null;
+        try {
+            Connection conn = DriverManager.getConnection(
+                    MariaDBProperties.DATABASE_URL + DB_NAME, MariaDBProperties.DATABASE_USER, MariaDBProperties.DATABASE_PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement(GET_AUTHOR_QUERY);
+            pstmt.setString(1, authorID);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                author = new Author(rs.getInt("authorID"), rs.getString("firstName"), rs.getString("lastName"));
+
+                System.out.println("Successfully retrieved authors!");
+            }
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error getting authors");
+        }
+
+        return author;
+    }
+
+    public static List<Author> getAllAuthors() {
+        String GET_AUTHORS_QUERY = "SELECT * FROM authors";
+        System.out.println(GET_AUTHORS_QUERY);
+
+        List<Author> authors = new ArrayList<Author>();
+        try {
+            Connection conn = DriverManager.getConnection(
+                    MariaDBProperties.DATABASE_URL + DB_NAME, MariaDBProperties.DATABASE_USER, MariaDBProperties.DATABASE_PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement(GET_AUTHORS_QUERY);
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                Author author = null;
+                author = new Author(rs.getInt("authorID"), rs.getString("firstName"), rs.getString("lastName"));
+                authors.add(author);
+
+                System.out.println("Successfully retrieved: " + rs.getString("firstName") + " " + rs.getString("lastName"));
+            }
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error getting authors");
+        }
+
+        return authors;
+    }
+
+    // UPDATE Methods
+    public static void updateBook(String isbn, Book book) {
+        String UPDATE_BOOK_QUERY = "UPDATE titles SET isbn = ?, title = ?, editionNumber = ?, copyright = ? WHERE isbn = ?";
+        System.out.println(UPDATE_BOOK_QUERY);
+
+        try {
+            Connection conn = DriverManager.getConnection(
+                    MariaDBProperties.DATABASE_URL + DB_NAME, MariaDBProperties.DATABASE_USER, MariaDBProperties.DATABASE_PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement(UPDATE_BOOK_QUERY);
+            pstmt.setString(1, book.getIsbn());
+            pstmt.setString(2, book.getTitle());
+            pstmt.setInt(3, book.getEditionNumber());
+            pstmt.setString(4, book.getCopyright());
+            pstmt.setString(5, isbn);
+            ResultSet rs = pstmt.executeQuery();
+
+            System.out.println("Successfully updated book!");
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error updating book");
+        }
+    }
+
+    public static void updateAuthor(String authorID, Author author) {
+        String UPDATE_BOOK_QUERY = "UPDATE authors SET authorID = ?, firstName = ?, lastName = ? WHERE authorID = ?";
+        System.out.println(UPDATE_BOOK_QUERY);
+
+        try {
+            Connection conn = DriverManager.getConnection(
+                    MariaDBProperties.DATABASE_URL + DB_NAME, MariaDBProperties.DATABASE_USER, MariaDBProperties.DATABASE_PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement(UPDATE_BOOK_QUERY);
+            pstmt.setInt(1, author.getAuthorID());
+            pstmt.setString(2, author.getFirstName());
+            pstmt.setString(3, author.getLastName());
+            pstmt.setString(4, authorID);
+            ResultSet rs = pstmt.executeQuery();
+
+            System.out.println("Successfully updated book!");
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error updating book");
+        }
+    }
+
+    // DELETE Methods
 }
