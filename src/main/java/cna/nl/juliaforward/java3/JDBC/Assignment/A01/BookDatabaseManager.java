3fa2943852b790538343567af7c38879baf51cbd
Forward <Julia.Forward@k202domain.com>
2025-02-03 12:29:42 -0330

    Added java docs to BDM

--- src/main/java/cna/nl/juliaforward/java3/JDBC/Assignment/A01/BookDatabaseManager.java
+++ src/main/java/cna/nl/juliaforward/java3/JDBC/Assignment/A01/BookDatabaseManager.java
@@ -1,16 +1,26 @@
 package cna.nl.juliaforward.java3.JDBC.Assignment.A01;
 
 import cna.nl.juliaforward.java3.JDBC.MariaDBProperties;
-import org.checkerframework.checker.units.qual.A;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Utility class for handling DB queries for working with a MariaDB database.
+ */
 public class BookDatabaseManager {
 
+    /**
+     * The name of the database to connect to.
+     */
     public static final String DB_NAME = "books";
 
+    /**
+     * Loads book and author data from a MariaDB database and populates the provided library.
+     *
+     * @param lib The library object to be populated with books, authors, and their relationships.
+     */
     public static void loadLibrary(Library lib) {
         String BOOKS_QUERY = "SELECT isbn, title, editionNumber, copyright FROM TITLES";
         String AUTHORS_QUERY = "SELECT authorID, firstName, lastName FROM AUTHORS";
@@ -21,91 +31,116 @@ public class BookDatabaseManager {
         // Load all books from the titles table
         try {
             Connection conn = DriverManager.getConnection(
-                    MariaDBProperties.DATABASE_URL + DB_NAME, MariaDBProperties.DATABASE_USER, MariaDBProperties.DATABASE_PASSWORD);
+                    MariaDBProperties.DATABASE_URL + DB_NAME,
+                    MariaDBProperties.DATABASE_USER,
+                    MariaDBProperties.DATABASE_PASSWORD);
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(BOOKS_QUERY);
 
-            // Extract all book data
+            // Extract and add books to the library
             while (rs.next()) {
-                Book currentBook = new Book(rs.getString("isbn"), rs.getString("title"), rs.getInt("editionNumber"), rs.getString("copyright"));
+                Book currentBook = new Book(
+                        rs.getString("isbn"),
+                        rs.getString("title"),
+                        rs.getInt("editionNumber"),
+                        rs.getString("copyright")
+                );
                 lib.getBookList().add(currentBook);
-
-//                System.out.println("=".repeat(20));
-//                System.out.println("ISBN: " + currentBook.getIsbn());
-//                System.out.println("Title: " + currentBook.getTitle());
-//                System.out.println("Edition Number: " + currentBook.getEditionNumber());
-//                System.out.println("Copyright: " + currentBook.getCopyright());
-
-                conn.close();
             }
+            conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        // Load all authors
+        // Load all authors from the authors table
         try {
             Connection conn = DriverManager.getConnection(
-                    MariaDBProperties.DATABASE_URL + DB_NAME, MariaDBProperties.DATABASE_USER, MariaDBProperties.DATABASE_PASSWORD);
+                    MariaDBProperties.DATABASE_URL + DB_NAME,
+                    MariaDBProperties.DATABASE_USER,
+                    MariaDBProperties.DATABASE_PASSWORD);
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(AUTHORS_QUERY);
 
-            // Extract all book data
+            // Extract and add authors to the library
             while (rs.next()) {
-                Author currentAuthor = new Author(rs.getInt("authorID"), rs.getString("firstName"), rs.getString("lastName"));
+                Author currentAuthor = new Author(
+                        rs.getInt("authorID"),
+                        rs.getString("firstName"),
+                        rs.getString("lastName")
+                );
                 lib.getAuthorList().add(currentAuthor);
-
-//                System.out.println("=".repeat(20));
-//                System.out.println("Author ID: " + currentAuthor.getAuthorID());
-//                System.out.println("First Name: " + currentAuthor.getFirstName());
-//                System.out.println("Last Name: " + currentAuthor.getLastName());
-
-                conn.close();
             }
+            conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        // Load relationships
+        // Load relationships between authors and books
         try {
             Connection conn = DriverManager.getConnection(
-                    MariaDBProperties.DATABASE_URL + DB_NAME, MariaDBProperties.DATABASE_USER, MariaDBProperties.DATABASE_PASSWORD);
+                    MariaDBProperties.DATABASE_URL + DB_NAME,
+                    MariaDBProperties.DATABASE_USER,
+                    MariaDBProperties.DATABASE_PASSWORD);
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(RELATIONSHIP_QUERY);
 
+            // Extract and establish relationships between books and authors
             while (rs.next()) {
                 int authorID = rs.getInt("authorID");
                 String isbn = rs.getString("isbn");
 
-                if (lib.getBook(isbn) != null) {
-                    Book currentBook = lib.getBook(isbn);
-                    currentBook.addAuthor(new Author(authorID, rs.getString("firstName"), rs.getString("lastName")));
+                // Retrieve book and author from the library
+                Book currentBook = lib.getBook(isbn);
+                Author currentAuthor = lib.getAuthor(authorID);
+
+                // Add relationships if the book and author exist
+                if (currentBook != null) {
+                    currentBook.addAuthor(new Author(
+                            authorID,
+                            rs.getString("firstName"),
+                            rs.getString("lastName")
+                    ));
                 }
 
-                if (lib.getAuthor(authorID) != null) {
-                    Author currentAuthor = lib.getAuthor(authorID);
-                    currentAuthor.addBook(new Book(isbn, rs.getString("title"), rs.getInt("editionNumber"), rs.getString("copyright")));
+                if (currentAuthor != null) {
+                    currentAuthor.addBook(new Book(
+                            isbn,
+                            rs.getString("title"),
+                            rs.getInt("editionNumber"),
+                            rs.getString("copyright")
+                    ));
                 }
-
-
             }
+            conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     // CREATE methods
+
+    /**
+     * Inserts a new book record into the database.
+     *
+     * @param book The book object containing the details of the book to be added.
+     */
     public static void createBook(Book book) {
         String CREATE_BOOK_QUERY = "INSERT INTO titles VALUES (?, ?, ?, ?)";
         System.out.println(CREATE_BOOK_QUERY);
         try {
             Connection conn = DriverManager.getConnection(
-                    MariaDBProperties.DATABASE_URL + DB_NAME, MariaDBProperties.DATABASE_USER, MariaDBProperties.DATABASE_PASSWORD);
+                    MariaDBProperties.DATABASE_URL + DB_NAME,
+                    MariaDBProperties.DATABASE_USER,
+                    MariaDBProperties.DATABASE_PASSWORD);
             PreparedStatement pstmt = conn.prepareStatement(CREATE_BOOK_QUERY);
+
+            // Set query parameters with book details
             pstmt.setString(1, book.getIsbn());
             pstmt.setString(2, book.getTitle());
             pstmt.setInt(3, book.getEditionNumber());
             pstmt.setString(4, book.getCopyright());
-            ResultSet rs = pstmt.executeQuery();
+
+            pstmt.executeUpdate();
 
             System.out.println("Created book successfully!");
             conn.close();
@@ -115,17 +150,27 @@ public class BookDatabaseManager {
         }
     }
 
+    /**
+     * Inserts a new author record into the database.
+     *
+     * @param author The Author object containing the details of the author to be added.
+     */
     public static void createAuthor(Author author) {
         String CREATE_AUTHOR_QUERY = "INSERT INTO authors VALUES (?, ?, ?)";
         System.out.println(CREATE_AUTHOR_QUERY);
         try {
             Connection conn = DriverManager.getConnection(
-                    MariaDBProperties.DATABASE_URL + DB_NAME, MariaDBProperties.DATABASE_USER, MariaDBProperties.DATABASE_PASSWORD);
+                    MariaDBProperties.DATABASE_URL + DB_NAME,
+                    MariaDBProperties.DATABASE_USER,
+                    MariaDBProperties.DATABASE_PASSWORD);
             PreparedStatement pstmt = conn.prepareStatement(CREATE_AUTHOR_QUERY);
+
+            // Set query parameters with author details
             pstmt.setInt(1, author.getAuthorID());
             pstmt.setString(2, author.getFirstName());
             pstmt.setString(3, author.getLastName());
-            ResultSet rs = pstmt.executeQuery();
+
+            pstmt.executeUpdate();
 
             System.out.println("Created author successfully!");
             conn.close();
@@ -134,6 +179,7 @@ public class BookDatabaseManager {
             System.out.println("Error creating author");
         }
     }
+
 
     public static void createRelation(Book book, Author author) {
         String CREATE_RELATION_QUERY = "INSERT INTO authorisbn VALUES (?, ?)";
@@ -147,6 +193,7 @@ public class BookDatabaseManager {
             ResultSet rs = pstmt.executeQuery();
 
             System.out.println("Created relation successfully!");
+            conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Error creating relation");
@@ -365,6 +412,7 @@ public class BookDatabaseManager {
             ResultSet rs = pstmt.executeQuery();
 
             System.out.println("Deleted relation successfully!");
+            conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Error deleting relation");
